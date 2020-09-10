6619bb7e26cc6efa255844306ba95f9500363610
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-10 13:43:39 -0500

    Stub customization

---
+++ app/Http/Controllers/Setting.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Setting extends Controller
+{
+    //
+    function index()
+    {
+    	//Write some code here
+    }
+
+}
---
+++ app/Http/Controllers/Youtube.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Youtube extends Controller
+{
+    //
+    function index()
+    {
+    	//Write some code here
+    }
+
+}
