15cbd021004c78da00f1d012b192e21590a1915c
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-10 19:46:39 -0500

    Laravel sanctum

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+         DB::table('users')->insert([
+    	'name' => 'anil sidhu',
+    	'email' => 'anil@sidhu.com',
+    	'password' => Hash::make('12345678')
+    	]);
+    }
+
+
+  
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware('auth:sanctum')->get('users', 'UserController@users');
+
+
+Route::post('login', 'UserController@index');
+Route::get('users', 'UserController@users');
